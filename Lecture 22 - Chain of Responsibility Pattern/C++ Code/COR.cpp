7eeb77b3c7d9890f688d9367d589e5528ab3673d
Relentless-ProCoder <pranayharishchandra@gmail.com>
2026-05-18 01:36:21 +0530

    L22

--- Lecture 22 - Chain of Responsibility Pattern/C++ Code/COR.cpp
+++ Lecture 22 - Chain of Responsibility Pattern/C++ Code/COR.cpp
@@ -2,49 +2,60 @@
 using namespace std;
 
 // Abstract Handler (Base Class)
-class MoneyHandler {
+class MoneyHandler
+{
 protected:
     MoneyHandler *nextHandler;
 
 public:
-    MoneyHandler() {
+    MoneyHandler()
+    {
         this->nextHandler = nullptr;
     }
 
-    void setNextHandler(MoneyHandler *next) { 
-        nextHandler = next; 
+    void setNextHandler(MoneyHandler *next)
+    {
+        nextHandler = next;
     }
 
     virtual void dispense(int amount) = 0;
 };
 
-class ThousandHandler : public MoneyHandler {
+class ThousandHandler : public MoneyHandler
+{
 private:
     int numNotes;
 
 public:
-    ThousandHandler(int numNotes) {
+    ThousandHandler(int numNotes)
+    {
         this->numNotes = numNotes;
     }
 
-    void dispense(int amount) override {
+    void dispense(int amount) override
+    {
         int notesNeeded = amount / 1000;
 
-        if(notesNeeded > numNotes) {
+        if (notesNeeded > numNotes)
+        {
             notesNeeded = numNotes;
             numNotes = 0;
         }
-        else {
+        else
+        {
             numNotes -= notesNeeded;
         }
 
-        if(notesNeeded > 0)
+        if (notesNeeded > 0)
             cout << "Dispensing " << notesNeeded << " x ₹1000 notes.\n";
 
         int remainingAmount = amount - (notesNeeded * 1000);
-        if(remainingAmount > 0) {
-            if(nextHandler != nullptr) nextHandler->dispense(remainingAmount);
-            else {
+        if (remainingAmount > 0)
+        {
+            if (nextHandler != nullptr)
+                nextHandler->dispense(remainingAmount);
+            else
+            {
                 cout << "Remaining amount of " << remainingAmount << " cannot be fulfilled (Insufficinet fund in ATM)\n";
             }
         }
@@ -52,33 +63,41 @@ public:
 };
 
 // Concrete Handler for 500 Rs Notes
-class FiveHundredHandler : public MoneyHandler {
+class FiveHundredHandler : public MoneyHandler
+{
 private:
     int numNotes;
 
 public:
-    FiveHundredHandler(int numNotes) {
-        this->numNotes = numNotes;    
+    FiveHundredHandler(int numNotes)
+    {
+        this->numNotes = numNotes;
     }
 
-    void dispense(int amount) override {
+    void dispense(int amount) override
+    {
         int notesNeeded = amount / 500;
 
-        if(notesNeeded > numNotes) {
+        if (notesNeeded > numNotes)
+        {
             notesNeeded = numNotes;
             numNotes = 0;
         }
-        else {
+        else
+        {
             numNotes -= notesNeeded;
         }
 
-        if(notesNeeded > 0)
+        if (notesNeeded > 0)
             cout << "Dispensing " << notesNeeded << " x ₹500 notes.\n";
 
         int remainingAmount = amount - (notesNeeded * 500);
-        if(remainingAmount > 0) {
-            if(nextHandler != nullptr) nextHandler->dispense(remainingAmount);
-            else {
+        if (remainingAmount > 0)
+        {
+            if (nextHandler != nullptr)
+                nextHandler->dispense(remainingAmount);
+            else
+            {
                 cout << "Remaining amount of " << remainingAmount << " cannot be fulfilled (Insufficinet fund in ATM)\n";
             }
         }
@@ -86,33 +105,41 @@ public:
 };
 
 // Concrete Handler for 200 Rs Notes
-class TwoHundredHandler : public MoneyHandler {
+class TwoHundredHandler : public MoneyHandler
+{
 private:
     int numNotes;
 
 public:
-    TwoHundredHandler(int numNotes) {
+    TwoHundredHandler(int numNotes)
+    {
         this->numNotes = numNotes;
     }
 
-    void dispense(int amount) override {
+    void dispense(int amount) override
+    {
         int notesNeeded = amount / 200;
 
-        if(notesNeeded > numNotes) {
+        if (notesNeeded > numNotes)
+        {
             notesNeeded = numNotes;
             numNotes = 0;
         }
-        else {
+        else
+        {
             numNotes -= notesNeeded;
         }
 
-        if(notesNeeded > 0)
+        if (notesNeeded > 0)
             cout << "Dispensing " << notesNeeded << " x ₹200 notes.\n";
 
         int remainingAmount = amount - (notesNeeded * 200);
-        if(remainingAmount > 0) {
-            if(nextHandler != nullptr) nextHandler->dispense(remainingAmount);
-            else {
+        if (remainingAmount > 0)
+        {
+            if (nextHandler != nullptr)
+                nextHandler->dispense(remainingAmount);
+            else
+            {
                 cout << "Remaining amount of " << remainingAmount << " cannot be fulfilled (Insufficinet fund in ATM)\n";
             }
         }
@@ -120,33 +147,41 @@ public:
 };
 
 // Concrete Handler for 100 Rs Notes
-class HundredHandler : public MoneyHandler {
+class HundredHandler : public MoneyHandler
+{
 private:
     int numNotes;
 
 public:
-    HundredHandler(int numNotes) {
+    HundredHandler(int numNotes)
+    {
         this->numNotes = numNotes;
     }
 
-    void dispense(int amount) override {
+    void dispense(int amount) override
+    {
         int notesNeeded = amount / 100;
 
-        if(notesNeeded > numNotes) {
+        if (notesNeeded > numNotes)
+        {
             notesNeeded = numNotes;
             numNotes = 0;
         }
-        else {
+        else
+        {
             numNotes -= notesNeeded;
         }
 
-        if(notesNeeded > 0)
+        if (notesNeeded > 0)
             cout << "Dispensing " << notesNeeded << " x ₹100 notes.\n";
 
         int remainingAmount = amount - (notesNeeded * 100);
-        if(remainingAmount > 0) {
-            if(nextHandler != nullptr) nextHandler->dispense(remainingAmount);
-            else {
+        if (remainingAmount > 0)
+        {
+            if (nextHandler != nullptr)
+                nextHandler->dispense(remainingAmount);
+            else
+            {
                 cout << "Remaining amount of " << remainingAmount << " cannot be fulfilled (Insufficinet fund in ATM)\n";
             }
         }
@@ -154,12 +189,13 @@ public:
 };
 
 // Client Code
-int main() {
+int main()
+{
     // Creating handlers for each note type
-    MoneyHandler* thousandHandler = new ThousandHandler(3);
-    MoneyHandler* fiveHundredHandler = new FiveHundredHandler(5);
-    MoneyHandler* twoHundredHandler= new TwoHundredHandler(10);
-    MoneyHandler* hundredHandler= new HundredHandler(20);
+    MoneyHandler *thousandHandler = new ThousandHandler(3);
+    MoneyHandler *fiveHundredHandler = new FiveHundredHandler(5);
+    MoneyHandler *twoHundredHandler = new TwoHundredHandler(10);
+    MoneyHandler *hundredHandler = new HundredHandler(20);
 
     // Setting up the chain of responsibility
     thousandHandler->setNextHandler(fiveHundredHandler);
